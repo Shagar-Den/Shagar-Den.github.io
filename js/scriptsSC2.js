e24d1d9da37e062e02cfbbaf56be85f31015f520
Shagar <sylvain.gaborit@depinfonancy.net>
2021-04-12 18:15:55 +0200

    Ball rebound on player + begin draw bricks

--- js/scriptsSC2.js
+++ js/scriptsSC2.js
@@ -1,35 +1,98 @@
-
-window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-                              window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-							  
 const terrain = document.getElementById('area');
 const player = document.getElementById('player');
 const ball = document.getElementById('ball');
 var borders = terrain.getBoundingClientRect();
 console.log('Border',borders.top, borders.right, borders.bottom, borders.left);
 
-player.posX = 0;
-player.posY = 0;
+
+player.posX = (borders.right + borders.left - (player.getBoundingClientRect().right + player.getBoundingClientRect().left)) / 2;
+player.posY = borders.bottom - player.getBoundingClientRect().bottom - 50;
+player.style.transform = `translate(${player.posX}px, ${player.posY}px)`;
 player.speed = 30;
 
-ball.posX = 0;
-ball.posY = 0;
-ball.speed = 0.05;
+ball.posX = (borders.right + borders.left - (ball.getBoundingClientRect().right + ball.getBoundingClientRect().left)) / 2;
+ball.posY = (borders.bottom + borders.top - (ball.getBoundingClientRect().bottom + ball.getBoundingClientRect().top)) / 2;
+ball.style.transform = `translate(${ball.posX}px, ${ball.posY}px)`;
+ball.speed = 0.1;
 ball.angle = Math.random() * 2 * Math.PI;
 
-const timerInterval = 110, borderX = 500;// borderY = 100;
+
+var score = 1;
+var horizontalSpeed = 0;
+
+
+
+var brickColumnCount = 4;
+var brickRowCount = 3;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 3;
+var brickOffsetLeft = (borders.right + borders.left)/2 - (brickColumnCount/2)*brickWidth;
+var brickOffsetTop = 30 + borders.top ;
+
+var bricks = [];
+for(var c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for(var r=0; r<brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1, element: null };
+    }
+}
+
+function addElement (c,r) {
+  var newDiv = document.createElement("div");
+  newDiv.classList.add('brick');
+  terrain.appendChild(newDiv);
+  bricks[c][r].element = newDiv;
+  bricks[c][r].x -= ((newDiv.getBoundingClientRect().right + newDiv.getBoundingClientRect().left) / 2);
+  bricks[c][r].y -= ((newDiv.getBoundingClientRect().top + newDiv.getBoundingClientRect().bottom) / 2);
+  newDiv.style.transform = `translate(${bricks[c][r].x}px, ${bricks[c][r].y}px)`;
+}
+
+
+function drawBricks() {
+  for(var c=0; c<brickColumnCount; c++) {
+      for(var r=0; r<brickRowCount; r++) {
+          if(bricks[c][r].status == 1) {
+              var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+              var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+              bricks[c][r].x = brickX;
+              bricks[c][r].y = brickY;
+              addElement (c,r);
+          }
+      }
+  }
+}
+
+drawBricks();
+
+const timerInterval = 110;
 const keys = [37, 39];
 var key = 0, timerId = 0;
+var running = true;
 
 function moveLeft(){
-  if(player.posX>-borderX){
-      player.posX -= player.speed;
+  var playerBound = player.getBoundingClientRect();
+  if(playerBound.left>borders.left){
+      if(playerBound.left - player.speed<borders.left){
+        player.posX -= playerBound.left-borders.left;
+      }
+      else{
+        player.posX -= player.speed;
+      }
+      horizontalSpeed = -1;
   }
 }
 
 function moveRight(){
-  if(player.posX<borderX){
-      player.posX += player.speed;
+  var playerBound = player.getBoundingClientRect();
+  if(playerBound.right<borders.right){
+      if(playerBound.right + player.speed>borders.right){
+        player.posX += borders.right - playerBound.right;
+      }
+      else{
+        player.posX += player.speed;
+      }
+      horizontalSpeed = 1;
   }
 }
 
@@ -81,12 +144,36 @@ function moveObject() {
     player.style.transform = `translate(${player.posX}px, ${player.posY}px)`;
 }
 
+function isInsidePlayer(pointX,pointY){
+  var playerBound = player.getBoundingClientRect();
+  if(pointY > playerBound.top && pointY < playerBound.bottom && pointX < playerBound.right && pointX > playerBound.left){
+    console.log('Inside player');
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+function isOutsideBox(pointX,pointY){
+  var borders = terrain.getBoundingClientRect();
+  if(pointY > borders.bottom || pointY < borders.top || pointX < borders.left || pointX > borders.right){
+    console.log('Outside box');
+    return true;
+  }
+  return false;
+}
+
+function needRebound(pointX,pointY){
+  return (isInsidePlayer(pointX,pointY) ||isOutsideBox(pointX,pointY));
+}
+
 function checkRebound(){
-	
+	var borders = terrain.getBoundingClientRect();
 	var ballBound = ball.getBoundingClientRect();
-	var playerBound = player.getBoundingClientRect();
 	
-	if(ballBound.right > borders.right && (ball.angle < Math.PI/2 || ball.angle > 3*Math.PI/2)){
+	//if(needRebound(ballBound.right, ((ballBound.top+ballBound.bottom)/2)) && (ball.angle < Math.PI/2 || ball.angle > 3*Math.PI/2)){
+  if((ballBound.right > borders.right || isInsidePlayer(ballBound.right, ((ballBound.top+ballBound.bottom)/2))) && (ball.angle < Math.PI/2 || ball.angle > 3*Math.PI/2)){
 		console.log('Ball',ballBound.top, ballBound.right, ballBound.bottom, ballBound.left)
 		if(ball.angle < Math.PI/2){
 			ball.angle += 2*((Math.PI/2)-ball.angle);
@@ -95,15 +182,18 @@ function checkRebound(){
 			ball.angle += 2*((3*Math.PI/2)-ball.angle);
 		}
 	}
-	if(ballBound.bottom > borders.bottom && ball.angle < Math.PI){
+	if((ballBound.bottom > borders.bottom || isInsidePlayer(((ballBound.right+ballBound.left)/2), ballBound.bottom)) && ball.angle < Math.PI){
+  //else if(needRebound(((ballBound.right+ballBound.left)/2), ballBound.bottom) && ball.angle < Math.PI){
 		console.log('Ball',ballBound.top, ballBound.right, ballBound.bottom, ballBound.left)
 		ball.angle += 2*(Math.PI-ball.angle);
 	}
-	if(ballBound.top < borders.top && ball.angle > Math.PI){
+  //else if(needRebound(((ballBound.right+ballBound.left)/2), ballBound.top) && ball.angle > Math.PI){
+	if((ballBound.top < borders.top || isInsidePlayer(((ballBound.right+ballBound.left)/2), ballBound.top)) && ball.angle > Math.PI){
 		console.log('Ball',ballBound.top, ballBound.right, ballBound.bottom, ballBound.left)
 		ball.angle += 2*(Math.PI-ball.angle);
 	}
-	if(ballBound.left < borders.left && (ball.angle > Math.PI/2 && ball.angle < 3*Math.PI/2)){
+  //else if(needRebound(ballBound.left, ((ballBound.top+ballBound.bottom)/2)) && (ball.angle > Math.PI/2 && ball.angle < 3*Math.PI/2)){
+	if((ballBound.left < borders.left || isInsidePlayer(ballBound.left, ((ballBound.top+ballBound.bottom)/2))) && (ball.angle > Math.PI/2 && ball.angle < 3*Math.PI/2)){
 		console.log('Ball',ballBound.top, ballBound.right, ballBound.bottom, ballBound.left)
 		if(ball.angle < Math.PI){
 			ball.angle += 2*((Math.PI/2)-ball.angle);
@@ -117,9 +207,9 @@ function checkRebound(){
 
 
 function update(progress) {
-	
+	//console.log("Progress: " + progress); 
 	ball.posX += ball.speed * Math.cos(ball.angle) * progress;
-	ball.posY += ball.speed * Math.sin(ball.angle) * progress;
+	ball.posY += ball.speed * Math.sin(ball.angle) * progress * score;
 	//console.log('A frame');
 	checkRebound();
 
@@ -129,7 +219,10 @@ function update(progress) {
 
 function loop(timestamp) {
   var progress = timestamp - lastRender
-  update(progress)
+
+  if(running){
+    update(progress);
+  }
 
   lastRender = timestamp
   window.requestAnimationFrame(loop)
@@ -138,4 +231,21 @@ var lastRender = 0
 window.requestAnimationFrame(loop)
 
 
+// Set global counter variable to verify event instances
+var nCounter = 0;
+
+// Set up event handler to produce text for the window focus event
+window.addEventListener("focus", function(event) 
+{ 
+    console.log("window has focus " + nCounter); 
+    nCounter = nCounter + 1;
+    running = true;
+}, false);
+
+// Example of the blur event as opposed to focus
+window.addEventListener("blur", function(event)
+{ 
+  console.log("window lost focus");
+  running = false;
+}, false);
 
