25634ca1685660db833646511480e48ace600b10
Shagar <sylvain.gaborit@depinfonancy.net>
2021-04-14 18:27:06 +0200

    Implement a API connected page on branch

---
+++ js/scriptsSC3.js
@@ -0,0 +1,17 @@
+var quoteOfTheDay = document.getElementById('quote');
+var quoterOfTheDay = document.getElementById('quoter');
+
+fetch("https://api.fisenko.net/quotes?l=en")
+  .then(response => response.json())
+  .then(response => {quoteOfTheDay.textContent = ' ' + response.text + ' - ', quoterOfTheDay.textContent = ' ' + response.author})
+  .catch((err) => {
+    console.log('Error: ' + err);
+  });
+
+/*
+//WORKS
+//fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits')
+fetch('https://api.taylor.rest/')
+  .then(response => response.json())
+  .then(response => {quoteOfTheDay.textContent = ' ' + response.quote + ' - ', quoterOfTheDay.textContent = ' ' + response.author});
+  */
