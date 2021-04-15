64efec037b1b6aa6c1e38d5201d768445083a72e
Shagar <sylvain.gaborit@depinfonancy.net>
2021-04-15 18:33:08 +0200

    Working quote of the day footer and first button API (bored answer)

--- js/scriptsSC3.js
+++ js/scriptsSC3.js
@@ -1,20 +1,69 @@
 var quoteOfTheDay = document.getElementById('quote');
 var quoterOfTheDay = document.getElementById('quoter');
+var apiAnswer1 = document.getElementById('firstapiAnswr');
+var allQuotes;
 
-fetch("https://api.fisenko.net/quotes?l=en", {
-	//method : 'GET',
-	headers: {"Referrer Policy": "strict-origin-when-cross-origin"}//"Access-Control-Allow-Origin": "*"}
-})
-  .then(response => {console.log(response),response.json()})
+
+var myHeadersForQuote = new Headers();
+//myHeadersForQuote.append("Access-Control-Allow-Origin", "*");
+/*myHeadersForQuote.append("content-type",	"application/json; charset=utf-8");*/
+var myInit = { method: 'GET',
+               headers: myHeadersForQuote,
+               mode: 'cors',
+               cache: 'default' };
+
+
+
+
+//FORBIDDEN ERROR or CORS ERROR ...
+/*
+fetch("https://cors-anywhere.herokuapp.com/https://api.fisenko.net/quotes?l=en",myInit)
+  .then(response => response.json())
   .then(response => {quoteOfTheDay.textContent = ' ' + response.text + ' - ', quoterOfTheDay.textContent = ' ' + response.author})
   .catch((err) => {
     console.log('Error: ' + err);
   });
+*/
 
 /*
+fetch('https://zenquotes.io/api/quotes',myInit)
+  .then(response => response.json())
+  .then(response => {quoteOfTheDay.textContent = ' ' + response.q + ' - ', quoterOfTheDay.textContent = ' ' + response.a});
+  */
+
+
+
+
+
+
 //WORKS
+/*
 //fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits')
-fetch('https://api.taylor.rest/')
+fetch('https://api.taylor.rest/',myInit)
   .then(response => response.json())
   .then(response => {quoteOfTheDay.textContent = ' ' + response.quote + ' - ', quoterOfTheDay.textContent = ' ' + response.author});
   */
+
+  fetch("https://type.fit/api/quotes")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    allQuotes = data;
+    var nb = Math.floor(Math.random() * data.length);
+    quoteOfTheDay.textContent = ' ' + data[nb].text, quoterOfTheDay.textContent = data[nb].author;
+  });
+
+// Blur event, opposed to focus (stop update when window not focused)
+window.addEventListener("blur", function(event)
+{ 
+  var nb = Math.floor(Math.random() * allQuotes.length);
+  quoteOfTheDay.textContent = ' ' + allQuotes[nb].text, quoterOfTheDay.textContent = allQuotes[nb].author;
+}, false);
+
+
+document.getElementById("firstapiBtn").addEventListener("click", function() {
+  fetch("https://www.boredapi.com/api/activity/")
+  .then(response => response.json())
+  .then(response => {apiAnswer1.textContent = response.activity});
+});
